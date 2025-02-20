6522b713c9987f6dc7391d63464d8f9a22616f50
jerzonhr <106408622+jerzonhr@users.noreply.github.com>
2025-02-20 14:18:45 -0600

    👌 IMPROVE: Add user model and implement login functionality in Master_View

---
+++ VEMWebAppDesign/Models/User.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace VEMWebAppDesign.Models;
+
+public class User
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
